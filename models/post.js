189bf5723dadba9bd59f2e2f766d188bd83a786a
Jordan Castling-Bolt <jordancastlingbolt@gmail.com>
2023-11-07 08:47:06 +0200

    update

--- models/post.js
+++ models/post.js
@@ -1,16 +1,31 @@
-// Import the mongoose library
 const mongoose = require('mongoose');
 
+// Enum for the government department names
+const departmentEnum = [
+  'Department of Education',
+  'Department of Health',
+  'Department of Transport',
+  'Department of Finance',
+  'Department of Justice',
+  'Department of Defence',
+  'Department of Labor',
+  'Department of Energy',
+  'Department of National Security',
+  'Department of State',
+];
+
 // Define the schema for the Post model
-// The schema outlines the shape of the documents within MongoDB collection
-const postschema = mongoose.Schema(
-    {
-        id: { type: String, required: true },  // Unique identifier for each post
-        title: {type: String, required: true }, //Post Title
-        post: { type: String, required: true } // Post content
-    }
-);
+const postSchema = mongoose.Schema({
+  id: { type: String, required: true }, // Unique identifier for each post
+  title: { type: String, required: true }, // Post Title
+  post: { type: String, required: true }, // Post content
+  // Define the department field with the enum, specific to government departments
+  department: { 
+    type: String, 
+    required: true, 
+    enum: departmentEnum // This restricts the department to one of the predefined government departments
+  }
+});
 
 // Export the Post model based on the schema
-// This will create a Mongoose model named 'Post' and it will use the schema defined above
-module.exports = mongoose.model('Posts', postschema);
+module.exports = mongoose.model('Posts', postSchema);
--- routes/post.js
+++ routes/post.js
@@ -2,21 +2,22 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
-const checkAuth = require('../check-auth');
+const checkAuth = require('../check-auth'); 
 
 // GET: Retrieve all posts (Requires Authentication)
 // This route is protected by the "checkAuth" middleware function
-router.get('', checkAuth, (req, res) => {
+router.get('/', checkAuth, (req, res) => {
     // Using Mongoose's find method to get all posts from the database
     Post.find()
-        .then((posts) => {
+        .select('id title post department') // Selecting the fields to return
+        .then(posts => {
             // Sending a successful response along with the list of posts
             res.json({
                 message: 'Posts found',
                 posts: posts
             });
         })
-        .catch((err) => {
+        .catch(err => {
             // Handling any errors that occur
             res.status(500).json({ error: err });
         });
@@ -24,25 +25,25 @@ router.get('', checkAuth, (req, res) => {
 
 // POST: Create a new post (Requires Authentication)
 // This route is protected by the "checkAuth" middleware function
-router.post('', checkAuth, (req, res) => {
+router.post('/', checkAuth, (req, res) => {
     // Creating a new Post document using the request's body data
     const post = new Post({
         id: req.body.id,
         title: req.body.title,
-        post: req.body.post,  
-        userId: req.userData.userId  
+        post: req.body.post,
+        department: req.body.department
     });
-    
+
     // Saving the newly created post to the database
     post.save()
-        .then((createdPost) => {
+        .then(createdPost => {
             // Sending a successful response along with the created post object
             res.status(201).json({
                 message: 'Post created',
                 post: createdPost
             });
         })
-        .catch((err) => {
+        .catch(err => {
             // Handling any errors that occur
             res.status(500).json({ error: err });
         });
@@ -50,27 +51,23 @@ router.post('', checkAuth, (req, res) => {
 
 // DELETE: Delete a post by its ID (Requires Authentication)
 // This route is protected by the "checkAuth" middleware function
-router.delete('', checkAuth, (req, res) => {
-    // Extracting the ID of the post to delete from the request's body
-    const idToDelete = req.body.id;
-    
-    // Using Mongoose's deleteOne method to delete the specified post
-    Post.deleteOne({ _id: idToDelete })
-        .then((result) => {
+router.delete('/:id', checkAuth, (req, res) => {
+    // Using Mongoose's deleteOne method to delete the specified post by its ID
+    Post.deleteOne({ _id: req.params.id })
+        .then(result => {
             // Checking if any documents were actually deleted
             if (result.deletedCount === 0) {
                 return res.status(404).json({ message: "Post not found" });
             }
-            
+
             // Sending a successful response
-            res.status(200).json({ message: "Post Deleted" });
+            res.status(200).json({ message: "Post deleted" });
         })
-        .catch((err) => {
+        .catch(err => {
             // Handling any errors that occur
             res.status(500).json({ error: err });
         });
 });
-
 
 // Exporting the router for use in other parts of the application
 module.exports = router;
